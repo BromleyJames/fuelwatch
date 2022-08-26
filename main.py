e880486e6149d5542c08ee8c8ccb7682e65aad19
James Bromley <james.bromley@servian.com>
2022-08-26 22:29:55 +0800

    added more boilerplate for useful variables

--- main.py
+++ main.py
@@ -3,3 +3,8 @@
 # Base URL - in future, import from the TOML file
 
 url_base = "https://www.fuelwatch.wa.gov.au/"
+historic_data_uri = "retail/historic"
+
+earliest_data = "2001-01"
+#Calculate the last calendar month from today's date
+latest_data  = None
