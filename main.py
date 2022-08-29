439e98579c2cc2540df6624476f8dcc7419e1eeb
James Bromley <james.bromley@servian.com>
2022-08-29 09:17:12 +0800

    Added a request to get the home page

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-
+import requests
 
 # Base URL - in future, import from the TOML file
 
 url_base = "https://www.fuelwatch.wa.gov.au/"
 historic_data_uri = "retail/historic"
+request_header = {
+    "User-Agent" :
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
+    }
 
 earliest_data = "2001-01"
 #Calculate the last calendar month from today's date
 latest_data  = None
+
+page = requests.get(url_base, headers=request_header)
+print(page.text)
